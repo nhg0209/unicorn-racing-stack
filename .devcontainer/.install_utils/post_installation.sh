5e8f140c23f6023092361b08dbb58f9680e42e76
Brojoon-10 <137401659+Brojoon-10@users.noreply.github.com>
2025-07-23 19:41:56 +0900

    Update post_installation.sh

--- .devcontainer/.install_utils/post_installation.sh
+++ .devcontainer/.install_utils/post_installation.sh
@@ -13,3 +13,19 @@ pip install ~/catkin_ws/src/race_stack/f110_utils/libs/ccma
 # build 
 cd /home/${USER}/catkin_ws
 catkin build
+
+# source for additional build
+source /opt/ros/noetic/setup.bash && source /home/${USER}/catkin_ws/devel/setup.bash
+
+# additional build 
+cd /home/${USER}/catkin_ws
+catkin build
+
+# f1tenth_simulator build
+cd /home/${USER}/catkin_ws
+catkin build f1tenth_simulator
+
+# last source
+source /opt/ros/noetic/setup.bash && source /home/${USER}/catkin_ws/devel/setup.bash
+
+
